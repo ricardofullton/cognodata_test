a74e52e2621c125f7b92e65daa7215b5d66ffabf
ricardofullton <ricardohuartesalazar@gmail.com>
2019-05-11 22:56:51 +0200

    first commit

---
+++ Cognodata_examen_Python_enunciado.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+"""
+EXAMEN PYTHON: El propos=3Dto de este examen es comprobar si el candidato posee =o=ocimientos de Python (básico y aplicados a ML)
+=nte=te usar Python 3 y puede usar las siguientes librer=as =salvo que se indique lo contrario): numpy, pandas, =atplotl=b.pyplot y sklearn
+"""
+
+
+"""
+BLOQUE = CONO=IMIENTOS GENERALES DE PYTHONb
+
+Pregunta 1.1: =rea una=20funcion a la cual le pases por parametros el =0D
+nume=o de filas y el numero de columnas y te la =ellene =on numeros aleatorios del 1 al 100 con 2 deci=ales
+C=ea una matriz de 10 filas y 7 columnas. (No=20utilices =UMPY ni PANDAS en este apartado, en los dem=C3�s puedes=20usarlo)
+"""
+
+
+"""
+Pregunta 1=2: A partir =e la matriz anterior crea un dataframe d= PANDAS, poniend= como nombre de columnas los días =e la semana
+=i no pudiste crear la matriz del apar=ado anterior, puede= crear una a partir de NUMPY
+"""=0D
+
+
+"""
+=regunta 1.3: Como crearías una =structura de datos si =l tipo de dato no fuera unifor=e (mismo tipo de dato)?=0ACrea una estructura cuyo pri=er elemento sea un vector =e NUMPY de 10 números =leatorios entre 1 y 100
+= el segundo elemento un v=ctor con esos mismos valores =ero como caracteres.
+"""=0D
+
+
+
+"""
+Pregunta =.4: normaliza todos los=20elementos de la primera matriz. E=to es, por
+cada =lemento resta la media y divide por=20la desviacion tipic= del conjunto.
+"""
+
+
+"""=0APregunta 1.5: Fi=tra la matriz normalizada y devuelve los=20valores entre =1 y 1, sin usar un bucle
+El resulta=o debe ser u= vector con dichos valores ordenados descend=ntemente
+""=
+
+
+"""
+BLOQUE 2: CREAR HISTOGRAM= Y DIAGRAM= DE BARRAS DE MÁS FRECUENTES.
+
+2.1 =rea un =ector de números enteros aleatorios entre el = y e= 10, de tamaño 100
+Crea otro vector de n=meros =leatorios entre el 11 y el 20 del mismo tamañ=
+= muestra por pantalla la tabla de frecuencia de la=20un=ón de los 2 vectores
+para los valores 5,6 y ==20(de la primera) y para los valores 14,16,19 (de la s==unda).
+"""
+
+
+"""
+2.2 crea el histograma p==a dicho vector de frecuencias, con ancho de barra 0.75, =3Dítulo: histograma
+y color de barra azul. Poner un=20=ítulo al eje x para diferenciar que son los n�=BAm=ros que se van a repetir.
+"""
+
+"""
+2.3 =rea=20un diagrama de barras para el vector fusionado
+c=n l=s 3 elementos más repetidos, si los hay, sin =ontar =a moda.
+"""
+
+
+
+"""
+BLOQUE 3: =ARGA DE=20FICHEROS Y MODELIZACION.
+
+Tenemos un ficher= llamado =ata_examen_sanit del cual queremos realizar una =odelizacion =ediante
+un arbol de decision y una regres=ón log�=ADstica.
+La columna objetivo de la tabla =e llama "tar=et", y el resto de columnas actuan como =nput para pred=cir el Target
+
+#3.1 Cargue dicho f=chero y prepara =os datos para la modelización.
+#=on una proporcion =0-30 para los conjuntos train y test=
+#Obtén los =stadisticos más importantes para =as variables del conju=to de datos
+"""
+
+
+=0A"""
+3.2 Entrene y =value los modelos. Arbol y reg.=20logística
+"""
+=0A
+
+"""
+3.3 Proponga =n método para comparar =mbos modelos (ej: Curva ROC)=0D
+"""
+
+=
+=
